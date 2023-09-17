012a6436b81af96394ae8d1760a69f1858d214c1
anastaskuz <anastaskuz16@yandex.ru>
2023-09-17 18:48:59 +0400

    'version1.0'

--- handlers.py
+++ handlers.py
@@ -3,8 +3,8 @@ from loguru import logger
 
 # from create_bot import DP
 from create_bot import BOT
-from keyboards import inline_kb_yon, inline_kb_start
-from data import MESSAGES, get_paper
+from keyboards import inline_kb_yon, inline_kb_start, kb_start
+from data import MESSAGES, QUESTIONS, get_paper
 
 # {id: [state(0, 1, 2, 3, 4), 'paper', 'paper', ...]}
 USERS = {}
@@ -15,79 +15,94 @@ USERS = {}
 async def command_start(message: types.Message):
     user_id = message.from_user.id
     if not (user_id in USERS.keys()):
-        USERS[user_id] = []
+        USERS[user_id] = [0]
         logger.info(f'Новый пользователь! {user_id}')
 
     msg = MESSAGES[0]
-    await BOT.send_message(message.from_user.id,
-                           msg, reply_markup=inline_kb_yon)
+    quest = QUESTIONS[0]
     USERS[user_id][0] = 0
+    await BOT.send_message(message.from_user.id,
+                           msg, reply_markup=kb_start)
+    await BOT.send_message(message.from_user.id,
+                           quest, reply_markup=inline_kb_yon, parse_mode="Markdown")
 
     logger.info(f'пользователь {user_id} ввел команду /start')
 
 
 # @DP.callback_query_handler(text='no')
 async def callback_button_no(callback_query: types.CallbackQuery):
+    await BOT.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     user_id = callback_query.from_user.id
     if USERS[user_id][0] == 0:
         msg = MESSAGES[1.0]
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, reply_markup=inline_kb_start)
 
     elif USERS[user_id][0] == 1:
         msg = MESSAGES[2.0]
+        quest = QUESTIONS[2.0]
         USERS[user_id][0] = 2.0
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_yon)
+                               msg, parse_mode="Markdown")
+        await BOT.send_message(callback_query.from_user.id,
+                               quest, reply_markup=inline_kb_yon, parse_mode="Markdown")
 
     elif USERS[user_id][0] == 2.0:
         msg = MESSAGES[4.0]
         USERS[user_id][0] = 0
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, reply_markup=inline_kb_start)
 
     elif USERS[user_id][0] == 2.1:
         msg = MESSAGES[3.0]
         USERS[user_id][0] = 0
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, reply_markup=inline_kb_start)
 
 
 # @DP.callback_query_handler(text='yes')
 async def callback_button_yes(callback_query: types.CallbackQuery):
+    await BOT.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     user_id = callback_query.from_user.id
     if USERS[user_id][0] == 0:
         msg = MESSAGES[1.1]
+        quest = QUESTIONS[1.1]
         USERS[user_id][0] = 1
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, parse_mode="Markdown")
+        await BOT.send_message(callback_query.from_user.id,
+                               quest, reply_markup=inline_kb_yon, parse_mode="Markdown")
 
     elif USERS[user_id][0] == 1:
         paper = get_paper()
         msg = MESSAGES[2.1].replace('PAPER', paper)
+        quest = QUESTIONS[2.1]
         USERS[user_id][0] = 2.1
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, parse_mode="Markdown")
+        await BOT.send_message(callback_query.from_user.id,
+                               quest, reply_markup=inline_kb_yon, parse_mode="Markdown")
 
     elif USERS[user_id][0] == 2.0:
         msg = MESSAGES[4.1]
         USERS[user_id][0] = 0
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, reply_markup=inline_kb_start)
 
     elif USERS[user_id][0] == 2.1:
         msg = MESSAGES[3.1]
         USERS[user_id][0] = 0
         await BOT.send_message(callback_query.from_user.id,
-                               msg, parse_mode="Markdown", reply_markup=inline_kb_start)
+                               msg, reply_markup=inline_kb_start)
 
 
 # @DP.callback_query_handler(text='start')
 async def callback_button_start(callback_query: types.CallbackQuery):
+    # await BOT.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     user_id = callback_query.from_user.id
     msg = MESSAGES[0]
     await BOT.send_message(callback_query.from_user.id,
-                           msg, reply_markup=inline_kb_yon)
+                           msg, reply_markup=inline_kb_yon, parse_mode="Markdown")
     USERS[user_id][0] = 0
 
     logger.info(f'пользователь {user_id} начал с начала')
--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,18 @@
-# from aiogram.types import KeyboardButton
+from aiogram.types import KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup
+
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.types import InlineKeyboardButton
 
+btn_start = KeyboardButton('/start')
 
 # создание кнопок с callback_data
 inline_btn_yes = InlineKeyboardButton('Да', callback_data='yes')
 inline_btn_no = InlineKeyboardButton('Нет', callback_data='no')
 inline_btn_start = InlineKeyboardButton('Начать с начала', callback_data='start')
 
+
+kb_start = ReplyKeyboardMarkup().add(btn_start)
 
 inline_kb_yon = InlineKeyboardMarkup().add(inline_btn_yes, inline_btn_no)
 inline_kb_start = InlineKeyboardMarkup().add(inline_btn_start)
